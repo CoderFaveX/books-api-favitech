92dc601d05b112ae7dcc0020dfbdfe6080a1caca
CoderFaveX <faveejiofor2009@gmail.com>
2025-01-16 02:16:47 +0100

    updating cors

--- js/index.mjs
+++ js/index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.mjs";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT ?? "3500";
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 dotenv.config();
 app.get("/", (req, res) => {
     res.json({ msg: "base api route" });
