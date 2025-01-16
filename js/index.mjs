e59bd1aa987099acb29aae3307833a500e0d01d9
CoderFaveX <faveejiofor2009@gmail.com>
2025-01-16 02:22:49 +0100

    modifying cors

--- js/index.mjs
+++ js/index.mjs
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import routes from "./routes/index.mjs";
 import cors from "cors";
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT ?? "3500";
 app.use(express.json());
-app.use(routes);
 app.use(cors());
-dotenv.config();
+app.use(routes);
 app.get("/", (req, res) => {
     res.json({ msg: "base api route" });
 });
